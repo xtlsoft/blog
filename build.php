6e41f888f1cfea0e7f8ce6bd104958986a6c5400
xtlsoft <xtlsoft@126.com>
2019-04-30 22:14:49 +0800

    [+] Finished

---
+++ build.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment;
+use League\CommonMark\Extras\CommonMarkExtrasExtension;
+use Symfony\Component\Yaml\Yaml;
+
+$environment = Environment::createCommonMarkEnvironment();
+$environment->addExtension(new CommonMarkExtrasExtension());
+$converter = new CommonMarkConverter([], $environment);
+
+echo shell_exec("mkdir dist && cd dist && git init && cd ..");
+
+function parseFile($name) {
+
+    global $converter;
+
+    if (is_dir($name)) {
+        foreach (glob($name . "/*") as $v) {
+            parseFile($v);
+        }
+        return;
+    }
+
+    if (substr($name, -3) !== ".md") return;
+    $job = substr($name, 10, -3);
+
+    $content = file_get_contents($name);
+    $content = explode("\n", $content);
+    if (trim($content[0]) !== "```yaml") {
+        echo "Build failed: $name has a mistake.\r\n";
+        exit(-1);
+    }
+    $conf = "";
+    $para = "";
+    $flag = false;
+    foreach ($content as $k=>$v) {
+        if ($k === 0) continue;
+        if ($flag) {
+            $para .= $v . "\n";
+            continue;
+        }
+        if (trim($v) === "```") {
+            $flag = true;
+        } else {
+            $conf .= $v . "\n";
+        }
+    }
+
+    $conf = Yaml::parse($conf);
+    $para = $converter->convertToHtml($para);
+    $conf["content"] = $para;
+
+    $tpl = file_get_contents("./template/" . $conf['template']);
+
+    foreach ($conf as $k=>$v) {
+        $tpl = str_replace('${'.$k.'}', $v, $tpl);
+    }
+
+    file_put_contents("./dist/" . $job . ".html", $tpl);
+    echo "Finished parsing: $name\r\n";
+    
+}
+
+foreach (glob("./content/*") as $v) {
+    parseFile($v);
+}
---
+++ content/index.md
@@ -0,0 +1,8 @@
+```yaml
+title: "xtlsoft :: Homepage"
+template: common.html
+```
+
+# xtlsoft :: Homepage
+
+[Home](/index.html) [About Me](/about.html) [Blog](https://blog.xtlsoft.top/)
