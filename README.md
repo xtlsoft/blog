87120e802627ff152354a6698e917da58fa0e24b
Tianle Xu <xtl@xtlsoft.top>
2019-07-21 12:27:14 +0800

    fix

--- README.md
+++
@@ -1,3 +0,0 @@
-# blog
-
-xtlsoft's technical blog.
--- template/footer.html.php
+++
@@ -1,9 +0,0 @@
-    </div>
-    <!-- <div class="mdui-color-blue-700" style="padding: 5px; height: 70px; margin-top: 20px; margin-bottom: 0px;">
-        <p align="center" class="mdui-text-color-silver-500">Developed by xtlsoft, theme using MDUI</p>
-        <p align="center" class="mdui-text-color-silver-500">&copy; 2016-<?= date("Y") ?> xtlsoft</p>
-    </div> -->
-    </body>
-    <script type="text/javascript" src="/highlight.js/highlight.min.js"></script>
-    <script type="text/javascript">hljs.initHighlightingOnLoad();</script>
-</html>
