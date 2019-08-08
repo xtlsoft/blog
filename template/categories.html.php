9744ee80a9791e561412af8d884d3c59768b8628
Tianle Xu <xtl@xtlsoft.top>
2019-08-08 20:09:54 +0800

    fix(comment): fix the bug in the previous commit

--- template/categories.html.php
+++ template/categories.html.php
@@ -1,11 +1,11 @@
-<?php include __DIR__ . "/header.html.php" ?>
-<?php global $categories ?>
+<?php include __DIR__ . "/header.html.php"; ?>
+<?php global $categories; ?>
 <div class="mdui-container" style="margin: 20px 0px;">
     <div class="mdui-row-xs-1 mdui-row-sm-2 mdui-row-md-3 mdui-row-lg-4 mdui-row-xl-5 mdui-grid-list">
-        <?php foreach ($categories as $i=>$n) : ?>
+        <?php foreach ($categories as $i => $n) : ?>
             <div class="mdui-col">
                 <div class="mdui-grid-tile mdui-ripple">
-                    <a href="/category/<?= $i ?>/1.html"><img src="https://cdn.jsdelivr.net/gh/idawnlight/typecho-theme-material@3.3.1/img/random/material-14.png"/></a>
+                    <a href="/category/<?= $i ?>/1.html"><img src="https://cdn.jsdelivr.net/gh/idawnlight/typecho-theme-material@3.3.1/img/random/material-14.png" /></a>
                     <div class="mdui-grid-tile-actions">
                         <div class="mdui-grid-tile-text">
                             <div class="mdui-grid-tile-title"><?= $n ?></div>
@@ -16,5 +16,5 @@
         <?php endforeach; ?>
     </div>
 </div>
-<?php include __DIR__ . "/copyright.html.php" ?>
-<?php include __DIR__ . "/footer.html.php" ?>
+<?php include __DIR__ . "/copyright.html.php"; ?>
+<?php include __DIR__ . "/footer.html.php"; ?>
